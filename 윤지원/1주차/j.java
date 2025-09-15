0e7e5e4e9c6b21b4e5bb98cb191e65a300079e7b
rosaze <geenieeyoon@gmail.com>
2025-09-15 19:11:54 +0900

    Feat: 1주차 알고리즘 과제 part 1- 재귀

---
+++ 윤지원/1주차/j.java
@@ -0,0 +1,25 @@
+import java.util.Scanner;
+
+public class j {
+    static int cnt;
+    public static int recursion(String s, int l, int r){
+        cnt++;
+        if(l >= r) return 1;
+        else if(s.charAt(l) != s.charAt(r)) return 0;
+        else return recursion(s, l+1, r-1);
+    }
+    public static int isPalindrome(String s){
+        return recursion(s, 0, s.length()-1);
+    }
+    public static void main(String[] args){
+        Scanner sc= new Scanner(System.in);
+        int t=sc.nextInt();
+        for (int i=0; i<t;i++){
+            String str = sc.next();
+            cnt=0;
+            System.out.println(isPalindrome(str)+" "+cnt);
+        }
+        sc.close();
+
+    }
+}
